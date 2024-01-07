d0f312347ae3322c918bd195f59a26a18afc5fe8
Samuel Lockton <lockton.sam@gmail.com>
2024-01-07 13:44:48 +1100

    button mobile fix?

--- mandel-workers.js
+++ mandel-workers.js
@@ -729,7 +729,7 @@ function gameloop() {
 		xnorm += ( xRate / zoom ) * ( Date.now() - time)  / 10;
 		ynorm += ( yRate / zoom ) * ( Date.now() - time ) / 10;
 		multiplier = -0.5 -Math.log2(((((xnorm - portalX)*zoom)/1600)**2 + ((ynorm-portalY)*zoom/1200)**2)**0.5);
-		contextM.fillText(multiplier,50,400);
+		contextM.fillText(multiplier,400,50);
 		zoom *= 1 + 0.01 * multiplier;
 		time = Date.now();
 		screenX = Math.round(-xnorm * zoom + canvasWidth/2);
