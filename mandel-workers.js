19ab02e4087f279e5078b03eb816b85fe076a8aa
Samuel Lockton <lockton.sam@gmail.com>
2024-03-20 15:08:25 +1100

    reset right and up

--- mandel-workers.js
+++ mandel-workers.js
@@ -716,8 +716,8 @@ document.ontouchstart = function(e){
     console.log('y ' + e.touches[0].clientY);
 	if ( (e.touches[0].clientX - (screen.width-screen.height/4))**2 + (e.touches[0].clientY - (screen.height*0.75))**2 < (screen.height/4)**2 ) {
 		console.log('inside circle!');
-		right = screen.height/4 / (e.touches[0].clientX - (screen.width-screen.height/4))
-		up = screen.height/4 / (e.touches[0].clientY - (screen.height*0.75))
+		right = (screen.height/4) / (e.touches[0].clientX - (screen.width-screen.height/4))
+		up = (screen.height/4) / (e.touches[0].clientY - (screen.height*0.75))
 	}
 }
 document.ontouchmove = function(e){
@@ -725,8 +725,8 @@ document.ontouchmove = function(e){
 	console.log('y ' + e.touches[0].clientY);
 	if ( (e.touches[0].clientX - (screen.width-screen.height/4))**2 + (e.touches[0].clientY - (screen.height*0.75))**2 < (screen.height/4)**2 ) {
 		console.log('inside circle!');
-		right = screen.height/4 / (e.touches[0].clientX - (screen.width-screen.height/4))
-		up = screen.height/4 / (e.touches[0].clientY - (screen.height*0.75))
+		right = (screen.height/4) / (e.touches[0].clientX - (screen.width-screen.height/4))
+		up = (screen.height/4) / (e.touches[0].clientY - (screen.height*0.75))
 	}
 }
 
@@ -734,14 +734,14 @@ document.ontouchcancel = function(e) {
 	console.log('x 0');
 	console.log('y 0');
 	right = 0;
-	left = 0;
+	up = 0;
 }
 
 document.ontouchend = function(e) {
 	console.log('x 0');
 	console.log('y 0');
 	right = 0;
-	left = 0;
+	up = 0;
 }
 
 // Game loop
