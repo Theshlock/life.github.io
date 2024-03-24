13ff5a2d2164d0b926ff313701242abd4435f949
Samuel Lockton <lockton.sam@gmail.com>
2024-03-25 10:54:35 +1100

    round values

--- mandel-workers.js
+++ mandel-workers.js
@@ -744,10 +744,10 @@ function gameloop() {
 		contextM.fillText("zoom mult.: " + Math.round(multiplier) ,300,550);
 		contextM.fillText((Date.now()-startTime-timePaused)/1000 + "s",100,550);
 		// accel show
-		contextM.fillText(acl.x,100,50);
-		contextM.fillText(acl.y,300,50);
-		contextM.fillText(acl.z,500,50);
-		
+		contextM.fillText(Math.round(acl.x),100,50);
+		contextM.fillText(Math.round(acl.y),300,50);
+		contextM.fillText(Math.round(acl.z),500,50);
+
 		zoom *= 1 + 0.01 * multiplier;
 		time = Date.now();
 		screenX = Math.round(-xnorm * zoom + canvasWidth/2);
