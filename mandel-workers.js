14d7bc76c624945e69ecc7da288b39a0fefdbc45
Samuel Lockton <lockton.sam@gmail.com>
2024-03-28 20:53:20 +1100

    removed debug

--- mandel-workers.js
+++ mandel-workers.js
@@ -763,10 +763,6 @@ function gameloop() {
 		contextM.fillText("level: " + level + "/8",500,550);
 		contextM.fillText("zoom mult.: " + Math.round(multiplier) ,300,550);
 		contextM.fillText((Date.now()-startTime-timePaused)/1000 + "s",100,550);
-		contextM.fillText(Math.round(tPSX),100,50);
-		contextM.fillText(Math.round(tPSY),200,50);
-		contextM.fillText(Math.round(tPMX),300,50);
-		contextM.fillText(Math.round(tPMY),400,50);
 		zoom *= 1 + 0.01 * multiplier;
 		time = Date.now();
 		screenX = Math.round(-xnorm * zoom + canvasWidth/2);
