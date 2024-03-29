3ec1f7ee13528e4ed51ff0df7678ec001e1a15ba
Samuel Lockton <lockton.sam@gmail.com>
2024-03-29 21:50:11 +1100

    trial and error

--- mandel-workers.js
+++ mandel-workers.js
@@ -556,8 +556,8 @@ var right = 0;
 window.onkeydown = function(event) {
     if (event.keyCode === 39) {right = 1}; 
     if (event.keyCode === 37) {right = -1}; 
-    if (event.keyCode === 38) {up = 1}; 
-    if (event.keyCode === 40) {up = -1}; 
+    if (event.keyCode === 38) {up = -1}; 
+    if (event.keyCode === 40) {up = 1}; 
 };
 
 window.onkeyup = function(event) {
